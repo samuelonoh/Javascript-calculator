add1d9365606d3f21144694df35fba9b4ef5ff2a
samuelonoh <samuelonoh12345@gmail.com>
2023-07-10 14:49:26 +0100

    light and dark mode

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-let evaluate = document.getElementById("evaluate")
-let result = document.getElementById("result")
+let evaluate = document.getElementById("evaluate");
+let result = document.getElementById("result");
+let modal = document.querySelector(".modal");
+let calSpace = document.querySelector(".calSpace");
+let calButtons = document.querySelector(".calButtons");
+let calculator = document.getElementById("calculator");
+let buttons = document.querySelectorAll("button");
+let history = document.querySelector(".history");
 
 function display(value) {
+    if (evaluate.innerText[0] === "0") {
+        evaluate.innerText = "";
+    }
     evaluate.innerText += value;
 }
 
@@ -10,7 +19,7 @@ function returnToZero() {
     result.innerText = "";
 }
 
-function del(){
+function del() {
     let str = evaluate.innerText.split("");
     str.pop();
     evaluate.innerText = str.join("");
@@ -20,8 +29,35 @@ function displayResult() {
     let eva = evaluate.innerText;
     let res = eval(eva);
     result.innerText = res;
-};
+}
 
 function displayModal() {
-    Modal.style.display = "block";
+    if (modal.style.display === "block") {
+        modal.style.display = "none";
+    } else {
+        modal.style.display = "block";
+    }
+}
+
+function darkMode() {
+    calSpace.style.background = "#36474f";
+    calSpace.style.color = "#f2f6f7";
+    calButtons.style.background = "#18212a";
+    for (const button of buttons) {
+        button.style.color = "#f2f6f7";
+    }
+}
+
+function lightMode() {
+    calSpace.style.background = "#ffffff";
+    calSpace.style.color = "#273238";
+    calButtons.style.background = "#f3f3f3";
+    for (const button of buttons) {
+        button.style.color = "#3c3c3c";
+    }
+}
+
+function displayHistory() {
+    let historyDiv = document.createElement("div");
+    history.style.height = "90vh";
 }
